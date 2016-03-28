706d69aa8c5238638e2896e888e6797876411d33
Nelson Solano <nelson.solano.hernandez@intel.com>
2016-03-28 15:29:19 -0600

    adding new message

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
+  file { '/etc/motd' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'games',
+    mode    => '0755',
+    content => "I really like Puppet\n",
+  }
 }
