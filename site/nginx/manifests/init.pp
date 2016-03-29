1cb6e46d9af122e2388c0e01e317985f76e9a2ff
Nelson Solano <nelson.solano.hernandez@intel.com>
2016-03-29 14:31:49 -0600

    nginx module

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,32 @@
+class nginx {
+  package { 'nginx' :
+    ensure => present,
+  }
+
+  service { 'nginx' :
+    ensure    => running,
+    enable    => true,
+  }
+
+  file { '/var/www' :
+    ensure => directory,
+  }
+
+  file { '/var/www/index.html' :
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { '/etc/nginx/nginx.conf' :
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d/default.conf' :
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf'
+    require => File['/etc/nginx/nginx.conf'],
+    notify  => Service['nginx'],
+  }
+}
