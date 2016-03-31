993549fb24888506b97d3d169a15776f89011289
nsolano2605 <nsolano2605@users.noreply.github.com>
2016-03-31 15:16:47 -0600

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,13 +1,15 @@
 class profile::wordpress {
   class { '::mysql::server' :  }
-
+  
+  class { '::mysql::client' :  }
+  
   class { '::wordpress' : 
-    install_dir => '/var/www/wordpress',
+    install_dir => '/var/www/html',
   }
 
   class { '::apache' : }
   
-  class {'::apache::mod::php': }
+  class {' ::apache::mod::php': }
 
   user { 'wordpress' :
     ensure => present,
